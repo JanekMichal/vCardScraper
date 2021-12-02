01e875d05eaac70a1b0a864b476ec55319584b21
Michał Jankowski <215744@edu.p.lodz.pl>
2021-12-02 14:40:26 +0100

    saving one vCard to one file

--- src/main/java/com/example/vCard/conrtoller/vCardScraper.java
+++ src/main/java/com/example/vCard/conrtoller/vCardScraper.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -44,28 +45,27 @@ public class vCardScraper {
             }
         }
 
-        StringBuilder vdfBuilder = new StringBuilder();
-        for (Company selectedCompany :
-                companies) {
-            vdfBuilder.append("BEGIN:VCARD\r\n");
-            vdfBuilder.append("VERSION:4.0\r\n");
-            vdfBuilder.append("ORG:").append(selectedCompany.getName()).append("\r\n");
-            vdfBuilder.append("TEL:").append(selectedCompany.getTelephone()).append("\r\n");
-            vdfBuilder.append("ADR:").append(selectedCompany.getStreet()).append(" ").append(selectedCompany.getPostalCode()).append("\r\n");
-            vdfBuilder.append("EMAIL:").append(selectedCompany.getEmail()).append("\r\n");
-            vdfBuilder.append("END:VCARD\n");
+        StringBuilder[] vcfBuilder = new StringBuilder[companies.size()];
+
+        for (int i = 0; i < companies.size(); i++) {
+            vcfBuilder[i] = new StringBuilder("BEGIN:VCARD\r\n");
+            vcfBuilder[i].append("VERSION:4.0\r\n");
+            vcfBuilder[i].append("ORG:").append(companies.get(i).getName()).append("\r\n");
+            vcfBuilder[i].append("TEL:").append(companies.get(i).getTelephone()).append("\r\n");
+            vcfBuilder[i].append("ADR:").append(companies.get(i).getStreet()).append(" ").append(companies.get(i).getPostalCode()).append("\r\n");
+            vcfBuilder[i].append("EMAIL:").append(companies.get(i).getEmail()).append("\r\n");
+            vcfBuilder[i].append("END:VCARD\n");
+
+            try {
+                File file = new File("vcard" + i + ".vcf");
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(vcfBuilder[i].toString().getBytes(StandardCharsets.UTF_8));
+            } catch (IOException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
         }
 
-
-        try {
-            File file = new File("vcard.vcf");
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            fileOutputStream.write(vdfBuilder.toString().getBytes(StandardCharsets.UTF_8));
-        } catch (IOException fileNotFoundException) {
-            fileNotFoundException.printStackTrace();
-        }
-
-        return vdfBuilder.toString();
+        return Arrays.toString(vcfBuilder);
     }
 
     private String getFieldFromJSONString(String json, String field) {
